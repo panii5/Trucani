f39ee9b87bdf587df151dbd58ad96d24311eb30e
panii5 <133724733+panii5@users.noreply.github.com>
2025-11-08 17:45:22 -0300

    Create nose.js

---
+++ nose.js
@@ -0,0 +1,14 @@
+const jugarButton = document.getElementById("jugar");
+    const configuracionButton = document.getElementById("configuracion");
+
+    // Eventos listos para usar más adelante
+    jugarButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      // TODO: Aquí vas a agregar la lógica para abrir el juego
+    });
+
+    configuracionButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      // TODO: Aquí vas a agregar la lógica para abrir el menú de configuraciones
+    });
+    
